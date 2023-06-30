1c146f88879ac0e9705a0bcfeb967e98a97b23f1
powerfulyang <i@powerfulyang.com>
2023-06-30 17:42:50 +0800

    hello

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod trie;
-mod hash;
-mod utils;
 mod backtracking;
+mod hash;
 mod heap;
+mod hello_world;
 mod matrix;
 mod stack;
+mod trie;
+mod utils;
 
 fn main() {
     println!("Hello, world!");
